454ad1ae8bb66c6ba1987b8dcdd65204638d14d7
eshakkhan29 <eshakkhan29@gmail.com>
2022-05-01 03:18:40 +0600

    update product responsive

--- src/components/ProductDetails/ProductDetails.js
+++ src/components/ProductDetails/ProductDetails.js
@@ -22,6 +22,8 @@ const ProductDetails = () => {
 
 
 
+    // delivery product
+
     const handelDelivery = async () => {
         const { quantity, ...rest } = product;
         const newQuantity = parseInt(quantity) - 1;
@@ -31,9 +33,9 @@ const ProductDetails = () => {
         setUpdateProduct(updatedProduct)
         console.log(updatedProduct);
 
-        const url = `https://fierce-everglades-14403.herokuapp.com/product/${productID}`;
-        const res = await axios.put(url, updateProduct);
-        console.log(res);
+        // const url = `https://fierce-everglades-14403.herokuapp.com/product/${productID}`;
+        // const res = await axios.put(url, updateProduct);
+        // console.log(res);
         setState(!state);
 
         // fetch(url, {
@@ -49,6 +51,8 @@ const ProductDetails = () => {
         //     })
     }
 
+    // reStock Product
+
     const handelRestock = () => {
         const increaseQuantity = quantityRef.current.value;
         const { quantity, ...rest } = product;
@@ -61,18 +65,18 @@ const ProductDetails = () => {
         setState(!state);
 
         const url = `https://fierce-everglades-14403.herokuapp.com/product/${productID}`;
-        fetch(url, {
-            method: 'PUT',
-            headers: {
-                'content-type': 'application/json'
-            },
-            body: JSON.stringify(updateProduct)
-        })
-            .then(res => res.json())
-            .then(data => {
-                console.log(data);
-                setState(!state);
-            })
+        // fetch(url, {
+        //     method: 'PUT',
+        //     headers: {
+        //         'content-type': 'application/json'
+        //     },
+        //     body: JSON.stringify(updateProduct)
+        // })
+        //     .then(res => res.json())
+        //     .then(data => {
+        //         console.log(data);
+        //         setState(!state);
+        //     })
     }
 
     return (
@@ -83,17 +87,17 @@ const ProductDetails = () => {
                         <img src={img} className="img-fluid rounded-3" alt="bike" />
                     </div>
                     <div className="col-lg-6">
-                        <div className="card-body p-0">
+                        <div className="card-body p-0 mt-lg-0 mt-3">
                             <h2 className="card-title">{name}</h2>
                             <p className="card-text">{description}</p>
                             <h4 className="card-text"><b>Supplier:</b> {supplier}</h4>
                             <h5 className="card-text"><b>Price:</b> ${price}</h5>
-                            <h5 className="card-text"><b> In stok:</b>  {quantity}</h5>
+                            <h5 className="card-text"><b> In stock:</b>  {quantity}</h5>
                         </div>
-                        <div className='d-flex justify-content-between mt-3'>
-                            <button onClick={handelDelivery} className='btn btn-dark'>Delivered</button>
+                        <div className='d-flex justify-content-lg-between align-items-center mt-3 flex-lg-row flex-column'>
+                            <button onClick={handelDelivery} className='btn btn-dark mb-lg-0 mb-3'>Delivered</button>
                             <div className='d-flex'>
-                                <input ref={quantityRef} type="text" name="restock" id="" placeholder='Stock Count' />
+                                <input className='ps-3' ref={quantityRef} type="text" name="restock" id="" placeholder='Stock Count' />
                                 <button onClick={handelRestock} className='btn btn-dark ms-3'>Restock</button>
                             </div>
                         </div>
